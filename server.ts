349db9c378454aea5d1da17547ef8898ad938d05
将之大西 <omccello@gmail.com>
2021-06-24 01:03:03 +0900

    fix

--- server.ts
+++ server.ts
@@ -1,15 +1,16 @@
 import express from 'express';
 import http from 'http';
+import { Server } from 'socket.io';
 
 const app: express.Express = express();
 const server = new http.Server(app);
-const io = require('socket.io')(server);
+const io = new Server(server);
 
 server.listen(process.env.PORT || 3030);
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.get('/', (_: any, res: any) => {
+app.get('/', (_: any, res: express.Response) => {
   res.render('room');
 });
