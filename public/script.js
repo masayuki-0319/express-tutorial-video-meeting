639dd6c49be99351381a9962cb5eebfc30209f88
将之大西 <omccello@gmail.com>
2021-06-24 23:02:52 +0900

    finish step8

--- public/script.js
+++ public/script.js
@@ -1,6 +1,27 @@
 const socket = io('/');
 const myPeer = new Peer();
-const userId = 12345;
+
+const videoWrap = document.getElementById('video-wrap');
+const myVideo = document.createElement('video');
+myVideo.muted = true;
+
+const addVideoStream = (video, stream) => {
+  video.srcObject = stream;
+  video.addEventListener('loadedmetadata', () => {
+    video.play();
+  });
+
+  videoWrap.append(video);
+};
+
+navigator.mediaDevices
+  .getUserMedia({
+    video: true,
+    audio: true,
+  })
+  .then((stream) => {
+    addVideoStream(myVideo, stream);
+  });
 
 myPeer.on('open', (userId) => {
   socket.emit('join-room', ROOM_ID, userId);
